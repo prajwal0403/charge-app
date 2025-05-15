7847146502377c788fd8b2a927662cb97a494c62
prajwal0403 <rautprajwal0403@gmail.com>
2025-05-15 14:21:43 +0530

    env fix

--- backend/config/config.js
+++ backend/config/config.js
@@ -1,19 +1,24 @@
 require('dotenv').config();
-
+PORT=5000
+DB_USER="postgres"
+DB_PASSWORD="Prajwal@0403"
+DB_NAME="postgres"
+DB_HOST="db.waauwzazgebvrxwefidt.supabase.co"
+JWT_SECRET="asdasdasdasd"
 module.exports = {
   development: {
-    username: process.env.DB_USER,
-    password: process.env.DB_PASSWORD,
-    database: process.env.DB_NAME,
-    host: process.env.DB_HOST,
+    username: "postgres",
+    password: "Prajwal@0403",
+    database: "postgres",
+    host: "db.waauwzazgebvrxwefidt.supabase.co",
     port: process.env.DB_PORT || 5432,
     dialect: 'postgres'
   },
   production: {
-    username: process.env.DB_USER,
-    password: process.env.DB_PASSWORD,
-    database: process.env.DB_NAME,
-    host: process.env.DB_HOST,
+    username: "postgres",
+    password: "Prajwal@0403",
+    database: "postgres",
+    host: "db.waauwzazgebvrxwefidt.supabase.co",
     port: process.env.DB_PORT || 5432,
     dialect: 'postgres'
   }
