a85b6d8e94e097a170832477c7fb6fa2b91d32d1
prajwal0403 <rautprajwal0403@gmail.com>
2025-05-15 15:15:09 +0530

    issues resolved

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App.jsx';
 import './index.css';
-import './app.css';
+import './App.css';
 import { BrowserRouter } from 'react-router-dom';
 import { MantineProvider } from '@mantine/core';
 import { Provider } from 'react-redux';
