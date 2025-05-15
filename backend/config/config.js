159bf02b4dfc8ec2a3112232176017c34ed5a6d2
prajwal0403 <rautprajwal0403@gmail.com>
2025-05-15 14:45:44 +0530

    fix

--- backend/config/config.js
+++ backend/config/config.js
@@ -11,7 +11,7 @@ module.exports = {
     dialectOptions: {
       ssl: {
         require: true,
-        rejectUnauthorized: false // For Supabase / cloud Postgres
+        rejectUnauthorized: false
       }
     }
   },
--- backend/models/index.js
+++ backend/models/index.js
@@ -1,7 +1,9 @@
-const fs      = require('fs');
-const path    = require('path');
+const fs = require('fs');
+const path = require('path');
 const Sequelize = require('sequelize');
-const config  = require(__dirname + '/../config/config.js')['development'];
+
+const env = process.env.NODE_ENV || 'development';
+const config = require(__dirname + '/../config/config.js')[env];
 
 const db = {};
 const sequelize = new Sequelize(config.database, config.username, config.password, config);
